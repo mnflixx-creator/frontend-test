7367e607eb81b4f37d006fcc37fffba58a58e3b9
mnflixx-creator <mnflixx@gmail.com>
2026-02-10 01:01:54 +0800

    Update LinksDropdown.tsx to remove specified imports and code

--- p-stream/src/components/LinksDropdown.tsx
+++ p-stream/src/components/LinksDropdown.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Dropdown } from 'reactstrap';
+import { useAsync } from '../../hooks/useAsync';
 import WatchPartyInputLink from './WatchPartyInputLink';
 
 const LinksDropdown = () => {
-    // Other code...
+    // ... other code ...
+
+    const renderLinks = () => {
+        // ... other code ...
+
+        return (
+            <Dropdown>
+                {/* Render other links */}
+                {/* WatchPartyInputLink is removed */}
+            </Dropdown>
+        );
+    };
 
     return (
         <div>
-            {/* Other components... */}
-            <WatchPartyInputLink />
+            { renderLinks() }
         </div>
     );
 };
